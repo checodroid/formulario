df9fafffc1951267f80de9f497f8e175c630d666
Eric <delabra32@gmail.com>
2023-12-02 10:38:02 -0600

    maqueta 1

---
+++ 1_vtas_fnzas.php
@@ -0,0 +1,221 @@
+<?php
+include_once('header.php');
+?>
+
+<body>
+
+    <div class="mx-5 mt-5">
+        <h4>SECCION 1</h4>
+        <h1>VENTAS Y MERCADOTECNIA</h1>
+    </div>
+
+    <form action="" class="m-5">
+
+        <!-- PREGUNTA 1 -->
+        <div class="p-5" style="background-color:aliceblue">
+
+            <h4 class="mb-3 ">Gestión de venta</h4>
+
+            <div class="form-check mx-4">
+                <input class="form-check-input" type="radio" name="" id="">
+                <label class="form-check-label" for="">
+                    No se cuenta con una labor de venta específica, solo la atención de clientes que nos buscan.
+                </label>
+            </div>
+            <div class="form-check mx-4">
+                <input class="form-check-input" type="radio" name="" id="">
+                <label class="form-check-label" for="">
+                    Se cuenta con los mínimos protocolos de venta, para conectar con clientes de manera presencial y virtual.
+                </label>
+            </div>
+            <div class="form-check mx-4">
+                <input class="form-check-input" type="radio" name="" id="">
+                <label class="form-check-label" for="">
+                    Solo se cuenta con seguimiento manual.
+                </label>
+            </div>
+            <div class="form-check mx-4">
+                <input class="form-check-input" type="radio" name="" id="">
+                <label class="form-check-label" for="">
+                    Se cuenta con un CRM para la integración y monitorio de los pacientes actuales y posibles nuevos.
+                </label>
+            </div>
+            <div class="form-check mx-4">
+                <input class="form-check-input" type="radio" name="" id="">
+                <label class="form-check-label" for="">
+                    Se cuenta con un CRM que es explotado y aprovechado al máximo nivel.
+                </label>
+            </div>
+        </div>
+        <!-- PREGUNTA 2 -->
+        <div class="p-5" style="background-color:antiquewhite">
+
+            <h4 Class="mb-3 ">Actividad Promocional</h4>
+
+            <div class="form-check mx-4">
+                <input class="form-check-input" type="radio" name="" id="">
+                <label class="form-check-label" for="">
+                    No se cuenta con actividad promocional.
+                </label>
+            </div>
+            <div class="form-check mx-4">
+                <input class="form-check-input" type="radio" name="" id="">
+                <label class="form-check-label" for="">
+                    Se cuenta con promoción solo en algunas fechas importantes.
+                </label>
+            </div>
+            <div class="form-check mx-4">
+                <input class="form-check-input" type="radio" name="" id="">
+                <label class="form-check-label" for="">
+                    Se cuenta con promoción fechas importantes y en servicios específicos a lo largo del año.
+                </label>
+            </div>
+            <div class="form-check mx-4">
+                <input class="form-check-input" type="radio" name="" id="">
+                <label class="form-check-label" for="">
+                    Se cuenta con un programa anual de promociones aprovechando las temporalidades de año.
+                </label>
+            </div>
+            <div class="form-check mx-4">
+                <input class="form-check-input" type="radio" name="" id="">
+                <label class="form-check-label" for="">
+                    Se cuenta con un programa anual de promociones además de un plan de lealtad con privilegios exclusivos.
+                </label>
+            </div>
+        </div>
+        <!-- PREGUNTA  -->
+        <div class="p-5" style="background-color:aliceblue">
+
+            <h4 class="mb-3 ">Gestión de venta</h4>
+
+            <div class="form-check mx-4">
+                <input class="form-check-input" type="radio" name="" id="">
+                <label class="form-check-label" for="">
+                    No se cuenta con una labor de venta específica, solo la atención de clientes que nos buscan.
+                </label>
+            </div>
+            <div class="form-check mx-4">
+                <input class="form-check-input" type="radio" name="" id="">
+                <label class="form-check-label" for="">
+                    Se cuenta con los mínimos protocolos de venta, para conectar con clientes de manera presencial y virtual.
+                </label>
+            </div>
+            <div class="form-check mx-4">
+                <input class="form-check-input" type="radio" name="" id="">
+                <label class="form-check-label" for="">
+                    Solo se cuenta con seguimiento manual.
+                </label>
+            </div>
+            <div class="form-check mx-4">
+                <input class="form-check-input" type="radio" name="" id="">
+                <label class="form-check-label" for="">
+                    Se cuenta con un CRM para la integración y monitorio de los pacientes actuales y posibles nuevos.
+                </label>
+            </div>
+            <div class="form-check mx-4">
+                <input class="form-check-input" type="radio" name="" id="">
+                <label class="form-check-label" for="">
+                    Se cuenta con un CRM que es explotado y aprovechado al máximo nivel.
+                </label>
+            </div>
+        </div>
+
+
+        <!-- <div class="row border_preguntas">
+
+        <div class="col-md-2 p-2"> 
+            <p class="pregunta">Gestión de venta</p>
+        </div>
+        <div class="col-md-2 p-2 border_respuestas">
+            <div class="form-check mx-4">
+              <input class="form-check-input" type="radio" name="s1p1" value="1" id="s1p1">
+              <label class="form-check-label" for="s1p1">
+                No se cuenta con una labor de venta específica, solo la atención de clientes que nos buscan.
+              </label>
+            </div>
+        </div>
+        <div class="col-md-2 p-2 border_respuestas">
+            <div class="form-check mx-4">
+              <input class="form-check-input" type="radio" name="s1p1" value="2" id="s1p1">
+              <label class="form-check-label" for="s1p1">
+              Se cuenta con los mínimos protocolos de venta, para conectar con clientes de manera presencial y virtual.
+              </label>
+            </div>
+        </div>
+        <div class="col-md-2 p-2 border_respuestas">
+            <div class="form-check mx-4">
+              <input class="form-check-input" type="radio" name="s1p1" value="3" id="s1p1">
+              <label class="form-check-label" for="s1p1">
+              Solo se cuenta con seguimiento manual.
+              </label>
+            </div>
+        </div>
+        <div class="col-md-2 p-2 border_respuestas">
+            <div class="form-check mx-4">
+              <input class="form-check-input" type="radio" name="s1p1" value="4" id="s1p1">
+              <label class="form-check-label" for="s1p1">
+              Se cuenta con un CRM para la integración y monitorio de los pacientes actuales y  posibles nuevos.
+              </label>
+            </div>
+        </div>
+        <div class="col-md-2 p-2 border_respuestas">
+            <div class="form-check mx-4">
+              <input class="form-check-input" type="radio" name="s1p1" value="5" id="s1p1">
+              <label class="form-check-label" for="s1p1">
+              Se cuenta con un CRM que es explotado y aprovechado al máximo nivel .
+              </label>
+            </div>
+        </div>
+
+
+        <div class="col-md-2 p-2 border_preguntas_medio"> 
+            <p class="pregunta">Actividad Promocional</p>
+        </div>
+        <div class="col-md-2 p-2 border_respuestas_medio">
+            <div class="form-check mx-4">
+              <input class="form-check-input" type="radio" name="s1p2" value="1" id="s1p2">
+              <label class="form-check-label" for="s1p2">
+              No se cuenta con actividad promocional.
+              </label>
+            </div>
+        </div>
+        <div class="col-md-2 p-2 border_respuestas_medio">
+            <div class="form-check mx-4">
+              <input class="form-check-input" type="radio" name="s1p2" value="2" id="s1p2">
+              <label class="form-check-label" for="s1p2">
+              Se cuenta con promoción solo en algunas fechas importantes.
+              </label>
+            </div>
+        </div>
+        <div class="col-md-2 p-2 border_respuestas_medio">
+            <div class="form-check mx-4">
+              <input class="form-check-input" type="radio" name="s1p2" value="3" id="s1p2">
+              <label class="form-check-label" for="s1p2">
+              Se cuenta con promoción fechas importantes y en servicios específicos a lo largo del año.
+              </label>
+            </div>
+        </div>
+        <div class="col-md-2 p-2 border_respuestas_medio">
+            <div class="form-check mx-4">
+              <input class="form-check-input" type="radio" name="s1p2" value="4" id="s1p2">
+              <label class="form-check-label" for="s1p2">
+              Se cuenta con un programa anual de promociones aprovechando las temporalidades de año.
+              </label>
+            </div>
+        </div>
+        <div class="col-md-2 p-2 border_respuestas_medio">
+            <div class="form-check mx-4">
+              <input class="form-check-input" type="radio" name="s1p2" value="5" id="s1p2">
+              <label class="form-check-label" for="s1p2">
+              Se cuenta con un programa anual de promociones además de un plan de lealtad con privilegios exclusivos.
+              </label>
+            </div>
+        </div>
+    </div> -->
+    </form>
+
+</body>
+
+<?php
+include('footer.php');
+?>
